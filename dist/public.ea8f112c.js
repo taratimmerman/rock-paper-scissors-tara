8ef3302fd4e1b197629430e35673806571755f18
taratimmerman <timmermantara@gmail.com>
2025-03-22 21:29:54 -0600

    RPST-0 deploy 4

--- dist/public.ea8f112c.js
+++
@@ -1,2 +0,0 @@
-document.addEventListener("DOMContentLoaded",()=>{let e=document.getElementById("message");e&&(e.textContent="Hello World!")});
-//# sourceMappingURL=public.ea8f112c.js.map
--- public.127ca221.js
+++
@@ -1,2 +0,0 @@
-document.addEventListener("DOMContentLoaded",()=>{console.log("DOM content loaded!");let e=document.getElementById("message");e&&(e.textContent="Hello World!")});
-//# sourceMappingURL=public.127ca221.js.map
