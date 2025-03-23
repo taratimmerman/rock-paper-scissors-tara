7df6b93ede5734ea039f8ba302e0602043df71f3
taratimmerman <timmermantara@gmail.com>
2025-03-22 21:35:05 -0600

    RPST-0 deploy 5

---
+++ public.127ca221.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",()=>{console.log("DOM content loaded!");let e=document.getElementById("message");e&&(e.textContent="Hello World!")});
+//# sourceMappingURL=public.127ca221.js.map
