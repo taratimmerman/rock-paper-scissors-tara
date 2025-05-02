59549e40a48c4a852efcd2837cdb6d9208d9fdfa
taratimmerman <timmermantara@gmail.com>
2025-05-01 20:07:00 -0600

    RPST-2 add state

--- src/model/model.ts
+++ src/model/model.ts
@@ -1,9 +1,26 @@
 export class Model {
-  getScore(key: "player" | "computer"): number {
+  private state = {
+    scores: {
+      player: 0,
+      computer: 0,
+    },
+  };
+
+  constructor() {
+    this.state.scores.player = this.getScoreFromStorage("player");
+    this.state.scores.computer = this.getScoreFromStorage("computer");
+  }
+
+  private getScoreFromStorage(key: "player" | "computer"): number {
     return parseInt(localStorage.getItem(`${key}Score`) || "0", 10);
   }
 
+  getScore(key: "player" | "computer"): number {
+    return this.state.scores[key];
+  }
+
   setScore(key: "player" | "computer", value: number): void {
+    this.state.scores[key] = value;
     localStorage.setItem(`${key}Score`, value.toString());
   }
 }
